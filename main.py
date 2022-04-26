c20cfaa10f6518e637a2fdd367b3f314d9672909
Ernest Torz <ernesttorz@gmail.com>
2022-04-26 15:16:26 +0200

    nowe

--- main.py
+++ main.py
@@ -33,16 +33,16 @@ class PriorityQueue:
         return self._list[-1]
 
     def get_r(self, nb):
-        return self._list[-1][0]
+        return self._list[-1][1]
 
 def read(nb):
     lst = []
     with open('data\\SCHRAGE' + str(nb) + '.DAT') as f:
         f.readline() #pominięcie n
         for i in f:
-            x, y, z = i.split()
-            lst.append([int(x), int(y), int(z)])
-    return sorted(lst, key=lambda i: i[0]) # sortowanie po r
+            n, x, y, z = i.split()
+            lst.append([int(n), int(x), int(y), int(z)])
+    return sorted(lst, key=lambda i: i[1]) # sortowanie po r
 
 def get_anwser(nb):
     with open('data\\CARLIER' + str(nb) + '.OUT') as f:
@@ -53,14 +53,14 @@ def schrage(data):
     t = 0
     k = 0
     b = 0 # indeks zadania z cmaxem w permutacji
-    N = PriorityQueue(itemgetter(0), True, [])
-    G = PriorityQueue(itemgetter(2), False, [])
+    N = PriorityQueue(itemgetter(1), True, [])
+    G = PriorityQueue(itemgetter(3), False, [])
 
     C_max = 0
     pi = []
 
     for i in range(0, len(data)):
-        N.add([int(data[i][0]), int(data[i][1]), int(data[i][2])])
+        N.add([int(data[i][0]), int(data[i][1]), int(data[i][2]), int(data[i][3])])
 
     while G.is_empty() is False or N.is_empty() is False:  # 2
         while N.is_empty() is False and int(N.get_r(0)) <= t:  # 3
@@ -73,9 +73,9 @@ def schrage(data):
             e = G.get_element()
             G.delete()
             pi.append(e)
-            t += int(e[1])
-            if C_max <= int(t+int(e[2])):
-                C_max = int(t+int(e[2]))
+            t += int(e[2])
+            if C_max <= int(t+int(e[3])):
+                C_max = int(t+int(e[3]))
                 b = k
             k += 1
             
@@ -86,27 +86,27 @@ def schrage(data):
 def schrage_div(data):
     n = len(data)
     t = 0                                           # total time
-    N = PriorityQueue(itemgetter(0), True, [])      # list of unordered tasks
-    G = PriorityQueue(itemgetter(2), False, [])     # list of ready to implementation tasks
+    N = PriorityQueue(itemgetter(1), True, [])      # list of unordered tasks
+    G = PriorityQueue(itemgetter(3), False, [])     # list of ready to implementation tasks
 
     C_max = 0
     pi = []
    
 
     for i in range(0, n):
-        N.add([int(data[i][0]), int(data[i][1]), int(data[i][2])])
+        N.add([int(data[i][0]), int(data[i][1]), int(data[i][2]), int(data[i][3])])
 
-    onMachine=[0, 0, 999999]                    # current task initialization
+    onMachine=[0, 0, 0, 999999]                    # current task initialization
 
     while G.is_empty() is False or N.is_empty() is False:   # check if at least one OR lists isn't empty
         while N.is_empty() is False and int(N.get_r(0)) <= t:  # check if N isn't empty AND availability time is less than total time
             e = N.get_element()                                # ready task
             G.add(e)
             N.delete()
-            if e[2] > onMachine[2]:                            # check if time to complete ready task is higher than time on machine
-                onMachine[1] = t - e[0]
-                t = e[0]
-                if onMachine[1] > 0:
+            if e[3] > onMachine[3]:                            # check if time to complete ready task is higher than time on machine
+                onMachine[2] = t - e[1]
+                t = e[1]
+                if onMachine[2] > 0:
                     G.add(onMachine)
 
         if G.is_empty() is True:                                     # 5
@@ -116,8 +116,8 @@ def schrage_div(data):
             G.delete()
             onMachine = e
             pi.append(e)
-            t += int(e[1])
-            C_max = max(C_max, int(t+int(e[2])))
+            t += int(e[2])
+            C_max = max(C_max, int(t+int(e[3])))
 
 
     return C_max
@@ -125,30 +125,30 @@ def schrage_div(data):
 def findA(lst, b, Cmax):
     sum = a = 0
     for i in range(0, b + 1):
-        sum += lst[i][1]
+        sum += lst[i][2]
 
-    while a < b and not Cmax == (lst[a][0] + lst[b][2] + sum):
-        sum -= lst[a][1]
+    while a < b and not Cmax == (lst[a][1] + lst[b][3] + sum):
+        sum -= lst[a][2]
         a += 1
     return a
 
 def findC(lst, a, b):
     for i in range(b - 1, a - 1, -1):
-        if lst[i][2] < lst[b][2]:
+        if lst[i][3] < lst[b][3]:
             return i
     return None
 
 def findRPQprim(lst, b, c):
-    rprim = lst[c + 1][0]
+    rprim = lst[c + 1][1]
     pprim = 0
-    qprim = lst[c + 1][2]
+    qprim = lst[c + 1][3]
 
     for i in range(c + 1, b + 1):
-        if lst[i][0] < rprim:
-            rprim = lst[i][0]
-        if lst[i][2] < qprim:
-            qprim = lst[i][2]
-        pprim += lst[i][1]
+        if lst[i][1] < rprim:
+            rprim = lst[i][1]
+        if lst[i][3] < qprim:
+            qprim = lst[i][3]
+        pprim += lst[i][2]
 
     return rprim, pprim, qprim
 
@@ -160,25 +160,25 @@ def carlier(lst):
         return Cmax,lst 
 
     rprim, pprim, qprim = findRPQprim(lst, b, c) # min r, max q, suma czasów wykonania zadań - w bloku (c+1, b)
-    r_saved = lst[c][0] # zapisz r
-    lst[c][0] = max(lst[c][0], rprim + pprim) # modyfikacja terminu dostępności zadania c, wymusi to jego późniejszą realizację, za wszystkimi zadaniami w bloku(c+1, b)
+    r_saved = lst[c][1] # zapisz r
+    lst[c][1] = max(lst[c][1], rprim + pprim) # modyfikacja terminu dostępności zadania c, wymusi to jego późniejszą realizację, za wszystkimi zadaniami w bloku(c+1, b)
     LB = schrage_div(copy.deepcopy(lst)) # sprawdzamy dolne ograniczenie schrage z podziałem  - dla wszystkich permutacji spełniających to wymaganie
 
     if LB < Cmax: # sprawdź czy rozwiązanie jest obiecujące
         pomCmax, pomlst = carlier(lst)
         Cmax = min(Cmax, pomCmax) # wywołaj carliera jeszcze raz dla nowego problemu
 
-    lst[c][0] = r_saved # odtwórz r
+    lst[c][1] = r_saved # odtwórz r
 
-    q_saved = lst[c][2]
-    lst[c][2] = max(lst[c][2], pprim + qprim) # wymuszenie aby zadanie c było wykonywane przed wszystkimi zadaniami w bloku (c+1, b)
+    q_saved = lst[c][3]
+    lst[c][3] = max(lst[c][3], pprim + qprim) # wymuszenie aby zadanie c było wykonywane przed wszystkimi zadaniami w bloku (c+1, b)
     LB = schrage_div(copy.deepcopy(lst)) # sprawdź czy taki problem jest obiecujący
 
     if LB < Cmax:
         pomCmax, pomlst = carlier(lst)
         Cmax = min(Cmax, pomCmax)
 
-    lst[c][2] = q_saved # przywróc q
+    lst[c][3] = q_saved # przywróc q
 
     return Cmax,lst 
 
